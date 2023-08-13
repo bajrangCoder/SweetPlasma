90a99b07821d0631d0df3b45179a6d2f88d3df44
Raunak Raj <nkray21111983@gmail.com>
2023-08-13 13:24:28 +0530

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import plugin from '../plugin.json';
+
+class AcodePlugin {
+
+  async init() {
+
+  }
+
+  async destroy() {
+
+  }
+}
+
+if (window.acode) {
+  const acodePlugin = new AcodePlugin();
+  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+    if (!baseUrl.endsWith('/')) {
+      baseUrl += '/';
+    }
+    acodePlugin.baseUrl = baseUrl;
+    await acodePlugin.init($page, cacheFile, cacheFileUrl);
+  });
+  acode.setPluginUnmount(plugin.id, () => {
+    acodePlugin.destroy();
+  });
+}
